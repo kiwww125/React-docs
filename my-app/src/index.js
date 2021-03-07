fd3b4f1a5add22dd2f9c8afaa2ee7fc288f4ab31
Leegilwon <kiwww125@u.sogang.ac.kr>
2021-03-07 16:53:54 +0900

    implement tic-tac-toe prototype

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,39 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button 
-                className="square" 
-                onClick={() => this.setState({ 
-                        value: "X" 
-                    })}
-            >
-            {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -56,14 +44,59 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history : [{
+                    squares : Array(9).fill(null)
+            }],
+            xIsNext : true
+        }
+    }
+
+    handleClick(i){
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if(squares[i] || calculateWinner(squares)){
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        //update states
+        this.setState({
+            history : history.concat([
+                {squares : squares
+            }]),
+            xIsNext : !this.state.xIsNext
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        
+        if(winner) {
+            status = `winner is ${winner}`;
+        }
+        else {
+            status = `next player is ` + (this.state.xIsNext ? "X" : "O");
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i)=>this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
@@ -71,25 +104,121 @@ class Game extends React.Component {
     }
 }
 
-class ShoppingList extends React.Component {
-    render() {
-        return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        )
-    }
-}
-
-
-// ========================================
 
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
+
+// // function Comment(props) {
+// //     return (
+// //       <div className="Comment">
+// //         <UserInfo user={props.author} />
+// //         <CommentText />
+// //         <CommentDate />
+// //       </div>
+// //     );
+// //   }
+
+// // function Avatar(props) {
+// //     return (
+// //         <img className="Avatar"
+// //             src={props.user.avatarUrl}
+// //             alt={props.user.name}
+// //         />
+// //     );
+// // }
+
+// // function UserInfo(props) {
+// //     return (
+// //         <div className="UserInfo">
+// //             <Avatar user={props.user}/>
+// //             <div className="UserInfo-name">
+// //                 {props.user.name}
+// //             </div>
+// //         </div>
+// //     );
+// // }
+// // function CommentText(props) {
+// //     return (
+// //         <div className="Comment-text">
+// //           {props.text}
+// //         </div>
+// //     );
+// // }
+
+// // function CommentDate(props){
+// //     return (
+// //     <div className="Comment-date">
+// //         {formatDate(props.date)}
+// //     </div>
+// //   );
+// // }
+
+// // //this가 없다면 scope가 이상해짐, class 외부의 props를 참조한다. 
+// // class Clock extends React.Component {
+// //     //Component 내부의 state가 변경되면, React가 이를 감지하며
+// //     //해당 Component instance를 다시 render한다.
+// //     constructor(props) {
+// //         super(props);
+// //         this.state = { date: new Date() };
+// //     }
+
+// //     //component 출력물이 DOM에 rendering 된 이후에 실행
+// //     componentDidMount() {
+// //         this.timerID = setInterval(
+// //             () => this.tick(),
+// //             1000
+// //         );
+// //     }
+
+// //     componentWillMount() {
+// //         clearInterval(this.timerID);
+// //     }
+
+// //     tick() {
+// //         this.setState({
+// //             date: new Date()
+// //         });
+// //     }
+
+// //     render() {
+// //         return (
+// //             <div>
+// //                 <h1>Hello, world!</h1>
+// //                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+// //             </div>
+// //         );
+// //     }
+// // }
+
+// // ReactDOM.render(
+// //     <Clock />,
+// //     document.getElementById('root')
+// // );
+
+
+
+
